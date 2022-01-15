19b4533245672fcaabb10187c928853baadd1ef8
zerdasoftware <zerdasoftware@gmail.com>
2022-01-15 22:38:37 +0300

    Add project files.

---
+++ login/Areas/Identity/Data/loginUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace login.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the loginUser class
+    public class loginUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName="nvarchar(100)")]
+        public string FirstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
